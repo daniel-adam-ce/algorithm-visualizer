7b1e69a927b3660a87b74a200e4c07137449c8a1
daniel-adam-ce <danieladamce@gmail.com>
2021-07-15 19:05:12 -0700

    resizing/viewport suport

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './styles/global.css'
 //import { useState } from 'react'
+import { useEffect } from 'react'
 import useState from 'react-usestateref'
 import Button from './components/Button'
 import binarySearch from './components/BinarySearch'
@@ -8,11 +9,11 @@ import quickSortMain from './components/Quicksort'
 let DELAY = 40;
 const MIN_VALUE_GENERATED = 10;
 const MAX_VALUE_GENERATED = 350;
-const WIDTH_CONSTANT = 1500;
+let WIDTH_CONSTANT = window.innerWidth-420;
 const HEIGHT_MULTIPLIER = 1.5;
 //const WIDTH_MULTIPLIER = 1.75;
 //let HEIGHT = HEIGHT_CONSTANT/(100*HEIGHT_MULTIPLIER);
-let WIDTH = WIDTH_CONSTANT/(50);
+let WIDTH = WIDTH_CONSTANT/(50)  >= 1 ? WIDTH_CONSTANT/(50) : 1;
 const generate = (min, max, num) => { 
   let array = [];
 
@@ -27,23 +28,54 @@ const generate = (min, max, num) => {
 }
 
 
+function useWindowSize(props) {
+  const [size, setSize] = useState ([window.innerHeight, window.innerWidth])
+  
+    useEffect(() => {
+      if (!props.displayState) {
+        const handleResize = () => {
+          setSize([window.innerHeight, window.innerWidth])
+          WIDTH_CONSTANT = window.innerWidth
+          WIDTH = 0.5*WIDTH_CONSTANT/(props.arrayRef.current.length)
+          props.font = WIDTH >= 30 ? 1 : 0
+          console.log(WIDTH, props.font)
+          props.setDisplayArray(props.arrayRef.current.map((value, index) => {
+            return <div className='bar' style = {{height: `${value*HEIGHT_MULTIPLIER}px`, width: `${WIDTH}px`}} key={index} id={index}><div className="bar-text" style={{ fontSize:`${props.font}rem`}}>{value}</div></div>
+          }))
+        }
+        window.addEventListener("resize", handleResize)
+        return () => {
+          window.removeEventListener('resize', handleResize)
+        }
+      }
+    }, [])
+  return size
+}
+
+function calcFontSize() {
+  let font = WIDTH >= 30 ? 1 : 0
+  return font
+}
+
 function App() {
   const [arraySize, setArraySize] = useState(50)
-  
   const [array, setArray, arrayRef] = useState(generate(MIN_VALUE_GENERATED,MAX_VALUE_GENERATED,arraySize))
-  let font = 0
-  if (arrayRef.current.length > 50) {
-    font = 0
-  } else {
-    font = 1
-  }
+  
+  let font = calcFontSize()
+  // if (arrayRef.current.length > 50) {
+  //   font = 0
+  // } else {
+  //   font = 1
+  // }
   const [arrayDisplay, setDisplayArray, arrayDisplayRef] = useState(array.map((value, index) => {
-    return <div className='bar' style = {{height: `${value*HEIGHT_MULTIPLIER}px`, width: `${WIDTH}px`}} key={index} id={index}><div className="bar-text" style={{ fontSize:`${font}`}}>{value}</div></div>
+    return <div className='bar' style = {{height: `${value*HEIGHT_MULTIPLIER}px`, width: `${WIDTH}px`}} key={index} id={index}><div className="bar-text" style={{ fontSize:`${font}rem`}}>{value}</div></div>
   }))
+  
   const [displayState, setDisplayState, displayStateRef] = useState(false)
   const [searchState, setSearchState] = useState(false)
   const [searchValue, setSearchValue, searchValueRef] = useState(0)
   const [isSorted, setIsSorted, setIsSortedRef] = useState(false)
+  const [displayHeight, displayWidth] = useWindowSize({array: array, arrayRef: arrayRef, setDisplayArray: setDisplayArray, font: font, displayState: displayState})
   //console.log(array)
   let sortProps = {
     font: font,
@@ -96,6 +128,7 @@ function App() {
     binarySearch(searchProps)
   }
   const clickGenerate = () =>{
+    console.log(window.innerHeight, window.innerWidth)
     DELAY = 10*(200/(arraySize))
     //HEIGHT = HEIGHT_CONSTANT/(arraySize*HEIGHT_MULTIPLIER)
     WIDTH = WIDTH_CONSTANT/(arraySize)
@@ -103,7 +136,7 @@ function App() {
     setSearchState(false)
     setIsSorted(false)
     setDisplayArray(arrayRef.current.map((value, index) => {
-      return <div className='bar' style = {{height: `${value*HEIGHT_MULTIPLIER}px`, width: `${WIDTH}px`}} key={index} id={index}><div className="bar-text" style={{ fontSize:`${arrayRef.current.length > 50 ? 0 : 1}rem`}}>{value}</div></div>
+      return <div className='bar' style = {{height: `${value*HEIGHT_MULTIPLIER}px`, width: `${WIDTH}px`}} key={index} id={index}><div className="bar-text" style={{ fontSize:`${calcFontSize()}rem`}}>{value}</div></div>
     }))
     //clickGenerateDisplay()
   }
@@ -111,7 +144,9 @@ function App() {
 
   return (
     <div className="main">
-      
+      {displayHeight}\n
+      {displayWidth}\n
+      {WIDTH_CONSTANT}
       <div className="buttons">
         <span>{displayState && !searchState && `Sorting ${array.length} elements`} {!displayState && !searchState && `${arraySize} elements`}  </span>
         {!displayState && <input type = "range" min ="10" max ="200" value = {arraySize} onChange = {(event)=>{
